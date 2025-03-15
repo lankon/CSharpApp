44f4c1ffe26f80ce76d312c206acc8f2049dad07
lankon <lankon850321@gmail.com>
2025-03-15 22:55:16 +0800

    Test

--- FileTransform/Wafer_Align_Angle/F_Wafer_Align_Angle.Designer.cs
+++ FileTransform/Wafer_Align_Angle/F_Wafer_Align_Angle.Designer.cs
@@ -88,7 +88,7 @@ namespace FileTransform.Wafer_Align_Angle
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 43);
             this.button1.TabIndex = 7;
-            this.button1.Text = "button1";
+            this.button1.Text = "Test";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
